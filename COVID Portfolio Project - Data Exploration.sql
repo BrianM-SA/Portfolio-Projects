74230ccd8f19de5ccdf65bd5ee0eabf727fb0af5
BrianM-SA <155302997+BrianM-SA@users.noreply.github.com>
2023-12-31 16:24:49 +0200

    Update COVID Portfolio Project - Data Exploration.sql

--- COVID Portfolio Project - Data Exploration.sql
+++ COVID Portfolio Project - Data Exploration.sql
@@ -10,9 +10,6 @@ From PortfolioProject..CovidDeaths
 Where continent is not null 
 order by 3,4
 
-
--- Select Data that we are going to be starting with
-
 Select Location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject..CovidDeaths
 Where continent is not null 
@@ -24,7 +21,7 @@ order by 1,2
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
-Where location like '%states%'
+Where location like '%South Africa%'
 and continent is not null 
 order by 1,2
 
@@ -34,7 +31,7 @@ order by 1,2
 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
+--Where location like '%South Africa%'
 order by 1,2
 
 
@@ -42,7 +39,7 @@ order by 1,2
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
+--Where location like '%South Africa%'
 Group by Location, Population
 order by PercentPopulationInfected desc
 
@@ -51,7 +48,7 @@ order by PercentPopulationInfected desc
 
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
+--Where location like '%South Africa%'
 Where continent is not null 
 Group by Location
 order by TotalDeathCount desc
@@ -64,7 +61,7 @@ order by TotalDeathCount desc
 
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
+
 Where continent is not null 
 Group by continent
 order by TotalDeathCount desc
@@ -75,7 +72,7 @@ order by TotalDeathCount desc
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
+
 where continent is not null 
 --Group By date
 order by 1,2
